e313e73d2fea3f8b65e8d5614769a049634a8eed
Matt Behrens <askedrelic@gmail.com>
2010-06-15 23:30:33 -0400

    Moving all configs to seperate files. Cleaning up Readme.

--- README.markdown
+++ README.markdown
@@ -7,9 +7,9 @@ Inspired by nWeb http://thehook.eu/tools/nweb/
 	sudo sh nweb-remix.sh
 	
 ## Details
-Designed for Debian 5.0 stable(lenny) while pulling the latest packges from testing.
+Designed for Debian 5.0 stable(lenny) while pulling the latest packages for nginx/mysql from testing and manually compiling PHP with FPM for FastCGI.
 
 * install nginx 0.7x from deb testing
-* install php 5.2.12 with php-fpm and apc from source
+* install php 5.2.12 with php-fpm from source
+* install APC from PEAR
 * install mysql 5.1.47 from deb testing
-
--- nweb-remix-php52.sh
+++ nweb-remix-php52.sh
@@ -1,5 +1,8 @@
-#setup apt testing
-echo "APT::Default-Release \"testing\";" >> /etc/apt/apt.conf
+#get current working dir
+cwd=$(pwd)
+
+#setup apt stable
+echo "APT::Default-Release \"stable\";" >> /etc/apt/apt.conf
 echo "deb http://http.us.debian.org/debian/ testing main contrib non-free" >> /etc/apt/sources.list
 echo "deb-src http://http.us.debian.org/debian/ testing main contrib non-free" >> /etc/apt/sources.list
 apt-get -y update
@@ -16,49 +19,11 @@ apt-get -y -t testing install htop less git-core rsync
 
 #edit nginx.conf HERE
 cat > /etc/nginx/nginx.conf <<heredoc
-user www-data;
-worker_processes  4;
-worker_rlimit_nofile 30000;
 
-error_log  /var/log/nginx/error.log;
-pid        /var/run/nginx.pid;
-
-events {
-    worker_connections  4096;
-}
-
-http {
-    include       /etc/nginx/mime.types;
-
-    access_log  /var/log/nginx/access.log;
-
-    sendfile        on;
-
-    keepalive_timeout  5;
-    tcp_nodelay        on;
-
-    gzip  on;
-    gzip_disable "MSIE [1-6]\.(?!.*SV1)";
-
-    include /etc/nginx/conf.d/*.conf;
-    include /etc/nginx/sites-enabled/*;
-}
 heredoc
 
 cat > /etc/nginx/sites-enabled/default <<heredoc
-server {
-    listen   80;
-    server_name  _;
-    root   /var/www;
-    index  index.php index.html index.htm;
 
-    location ~ \.php\$ {
-             fastcgi_pass   127.0.0.1:9000;
-             fastcgi_index  index.php;
-             fastcgi_param  SCRIPT_FILENAME  \$document_root\$fastcgi_script_name;
-             include        fastcgi_params;
-    }
-}
 heredoc
 
 #config nginx, prepend params
@@ -116,159 +81,7 @@ echo "apc.stat = 0" >> /etc/php/php.ini
 
 #edit php-fpm.ini HERE
 cat > /etc/php/php-fpm.conf <<heredoc
-<?xml version="1.0" ?>
-<configuration>
 
-    All relative paths in this config are relative to php's install prefix
-
-    <section name="global_options">
-
-        Pid file
-        <value name="pid_file">/usr/local/logs/php-fpm.pid</value>
-
-        Error log file
-        <value name="error_log">/usr/local/logs/php-fpm.log</value>
-
-        Log level
-        <value name="log_level">notice</value>
-
-        When this amount of php processes exited with SIGSEGV or SIGBUS ...
-        <value name="emergency_restart_threshold">10</value>
-
-        ... in a less than this interval of time, a graceful restart will be initiated.
-        Useful to work around accidental curruptions in accelerator's shared memory.
-        <value name="emergency_restart_interval">1m</value>
-
-        Time limit on waiting child's reaction on signals from master
-        <value name="process_control_timeout">5s</value>
-
-        Set to 'no' to debug fpm
-        <value name="daemonize">yes</value>
-
-    </section>
-
-    <workers>
-
-        <section name="pool">
-
-            Name of pool. Used in logs and stats.
-            <value name="name">default</value>
-
-            Address to accept fastcgi requests on.
-            Valid syntax is 'ip.ad.re.ss:port' or just 'port' or '/path/to/unix/socket'
-            <value name="listen_address">127.0.0.1:9000</value>
-
-            <value name="listen_options">
-
-                Set listen(2) backlog
-                <value name="backlog">-1</value>
-
-                Set permissions for unix socket, if one used.
-                In Linux read/write permissions must be set in order to allow connections from web server.
-                Many BSD-derrived systems allow connections regardless of permissions.
-                <value name="owner">www-data</value>
-                <value name="group">www-data</value>
-                <value name="mode">0666</value>
-            </value>
-
-            Additional php.ini defines, specific to this pool of workers.
-            <value name="php_defines">
-        <!--        <value name="sendmail_path">/usr/sbin/sendmail -t -i</value>        -->
-        <!--        <value name="display_errors">0</value>                              -->
-            </value>
-
-            Unix user of processes
-            <value name="user">www-data</value>
-
-            Unix group of processes
-            <value name="group">www-data</value>
-
-            Process manager settings
-            <value name="pm">
-
-                Sets style of controling worker process count.
-                Valid values are 'static' and 'apache-like'
-                <value name="style">static</value>
-
-                Sets the limit on the number of simultaneous requests that will be served.
-                Equivalent to Apache MaxClients directive.
-                Equivalent to PHP_FCGI_CHILDREN environment in original php.fcgi
-                Used with any pm_style.
-                <value name="max_children">32</value>
-
-                Settings group for 'apache-like' pm style
-                <value name="apache_like">
-
-                    Sets the number of server processes created on startup.
-                    Used only when 'apache-like' pm_style is selected
-                    <value name="StartServers">20</value>
-
-                    Sets the desired minimum number of idle server processes.
-                    Used only when 'apache-like' pm_style is selected
-                    <value name="MinSpareServers">5</value>
-
-                    Sets the desired maximum number of idle server processes.
-                    Used only when 'apache-like' pm_style is selected
-                    <value name="MaxSpareServers">35</value>
-
-                </value>
-
-            </value>
-
-            The timeout (in seconds) for serving a single request after which the worker process will be terminated
-            Should be used when 'max_execution_time' ini option does not stop script execution for some reason
-            '0s' means 'off'
-            <value name="request_terminate_timeout">0s</value>
-
-            The timeout (in seconds) for serving of single request after which a php backtrace will be dumped to slow.log file
-            '0s' means 'off'
-            <value name="request_slowlog_timeout">0s</value>
-
-            The log file for slow requests
-            <value name="slowlog">logs/slow.log</value>
-
-            Set open file desc rlimit
-            <value name="rlimit_files">4096</value>
-
-            Set max core size rlimit
-            <value name="rlimit_core">0</value>
-
-            Chroot to this directory at the start, absolute path
-            <value name="chroot"></value>
-
-            Chdir to this directory at the start, absolute path
-            <value name="chdir"></value>
-
-            Redirect workers' stdout and stderr into main error log.
-            If not set, they will be redirected to /dev/null, according to FastCGI specs
-            <value name="catch_workers_output">yes</value>
-
-            How much requests each process should execute before respawn.
-            Useful to work around memory leaks in 3rd party libraries.
-            For endless request processing please specify 0
-            Equivalent to PHP_FCGI_MAX_REQUESTS
-            <value name="max_requests">0</value>
-
-            Comma separated list of ipv4 addresses of FastCGI clients that allowed to connect.
-            Equivalent to FCGI_WEB_SERVER_ADDRS environment in original php.fcgi (5.2.2+)
-            Makes sense only with AF_INET listening socket.
-            <value name="allowed_clients">127.0.0.1</value>
-
-            Pass environment variables like LD_LIBRARY_PATH
-            All \$VARIABLEs are taken from current environment
-            <value name="environment">
-                <value name="HOSTNAME">\$HOSTNAME</value>
-                <value name="PATH">/usr/local/bin:/usr/bin:/bin</value>
-                <value name="TMP">/tmp</value>
-                <value name="TMPDIR">/tmp</value>
-                <value name="TEMP">/tmp</value>
-                <value name="OSTYPE">\$OSTYPE</value>
-                <value name="MACHTYPE">\$MACHTYPE</value>
-                <value name="MALLOC_CHECK_">2</value>
-            </value>
-        </section>
-    </workers>
-</configuration>
 heredoc
 
 #linux network tweaks for more connections
